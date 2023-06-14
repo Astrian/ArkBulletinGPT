07df5cf8312c4feacdff729052989e4c3b9775fc
Astrian Zheng <happy.wheel6743@astrian.moe>
2023-06-14 11:06:30 +1000

    Update for GPT 3.5 Turbo 16K Model

--- app.ts
+++ app.ts
@@ -45,28 +45,7 @@ async function refresh() {
       print("Content length: " + content.length)
 
       // GPT analysis
-      // Cut the HTML content into 4000-character chips
-      let chips = []
-      for (let i = 0; i < content.length; i += 5000) {
-        chips.push(content.slice(i, i + 5000))
-      }
-      // Recursion calling
-      let summaryList: string[] = []
-      for (let chip of chips) {
-        let chip_analysis = await functions.gpt_analysis(chip)
-        for (let event of chip_analysis.events) {
-          print("Updating event: " + event.name)
-          await functions.events_update(event)
-        }
-        summaryList.push(chip_analysis.summary)
-      }
-      let summary = ""
-      // If there are multiple summaries, give GPT a task for summary from those summaries
-      if (summaryList.length > 1) {
-        summary = await functions.gpt_summary_organizer(summaryList)
-      } else {
-        summary = summaryList[0]
-      }
+      let analysis_result = await functions.gpt_analysis(content)
 
       // Telegraph post
       let push_url = await functions.telegraph_post(content.replace(/\&/g, '%26'), response.data.announceList[i].webUrl, response.data.announceList[i].title.replace(/[\r\n]/g,""))
@@ -74,7 +53,7 @@ async function refresh() {
       // Telegram bot push
       let bot = new Bot(process.env.ARK_TELEGRAM_BOT_TOKEN ?? "")
       let msg_content = ""
-      if (summary !== "" && summary) msg_content = `<b>新游戏内公告</b>：${push_url}\n省流：${summary}\n${response.data.announceList[i].group === 'SYSTEM' ? '#系统公告' : '#活动通知'}`
+      if (analysis_result.summary !== "" && analysis_result.summary) msg_content = `<b>新游戏内公告</b>：${push_url}\n省流：${analysis_result.summary}\n${response.data.announceList[i].group === 'SYSTEM' ? '#系统公告' : '#活动通知'}`
       else msg_content = `<b>新游戏内公告</b>：${push_url}\n${response.data.announceList[i].group === 'SYSTEM' ? '#系统公告' : '#活动通知'}`
       print(msg_content)
       await bot.api.sendMessage(
--- functions/gpt_analysis.ts
+++ functions/gpt_analysis.ts
@@ -7,16 +7,17 @@ import axios from 'axios'
 // Initial
 let print = Debug('abg:functions/gpt_analysis.ts')
 
-const gpt_analysis = async (content: string): Promise<{summary: string, events: GameEvent[]}> => {
+const gpt_analysis = async (content: string): Promise<{events: GameEvent[], maintance: GameEvent[], summary: string}> => {
   print('gpt_analysis()')
 
   // Get now time
   let now = new Date()
   // Format time as “2023 年 1 月 1 日 04:00”
-  let now_time = now.getFullYear() + ' 年 ' + (now.getMonth() + 1) + ' 月 ' + now.getDate() + ' 日 ' + now.getHours() + ':' + now.getMinutes()
+  // let now_time = now.getFullYear() + ' 年 ' + (now.getMonth() + 1) + ' 月 ' + now.getDate() + ' 日 ' + now.getHours() + ':' + now.getMinutes()
+  let now_time = "2023 年 04 月 22 日"
 
   // Set prompt
-  let prompt = `以下是手游《明日方舟》的公告网页 HTML 的片段，请帮我从里面提取活动/维护计划的名称、起止时间和详细信息，以JSON 格式输出。
+  let prompt = `以下是手游《明日方舟》的公告网页 HTML，请帮我从里面提取活动/维护计划的名称、起止时间和详细信息，并再用一段话总结公告内容。
   
   一些注意事项：
 
@@ -26,12 +27,14 @@ const gpt_analysis = async (content: string): Promise<{summary: string, events: 
   - 公告中有可能不包含活动/维护计划有效信息，如遇此情况，请返回空数组
   - 活动/维护计划详情请你进行精炼与总结
   - 如果活动与「危机合约」有关，请在活动名称中标注「危机合约」字样
+  - 如果公告中只有一张图片，events 和 maintance 请返回空数组，summary 请返回空字符串
+
+  使用以下 JSON 模板输出：
   
-  使用以下 JSON 模板：
-  {"events": [{"name": "", "start_time": {"year": 2023, "month": 1, "day": 1, "hour": 4, "minute": 0}, "end_time": {"year": 2023, "month": 1, "day": 15, "hour": 3, "minute": 59}, "detail": ""}], "maintance": [{...}]}
+  {"events": [{"name": "", "start_time": {"year": 2023, "month": 1, "day": 1, "hour": 4, "minute": 0}, "end_time": {"year": 2023, "month": 1, "day": 15, "hour": 3, "minute": 59}, "detail": ""}], "maintance": [{...}], "summary": ""}
   `
   let body = {
-    "model": "gpt-3.5-turbo-0301",
+    "model": "gpt-3.5-turbo-16k",
     "messages": [
       {
         "role": "system",
@@ -43,61 +46,28 @@ const gpt_analysis = async (content: string): Promise<{summary: string, events: 
       }
     ]
   }
-  let events: GameEvent[] = []
+  var result: {
+    events: GameEvent[],
+    maintance: GameEvent[],
+    summary: string
+  } = {
+    events: [],
+    maintance: [],
+    summary: ""
+  }
   try {
     let events_response = await axios.post('https://api.openai.com/v1/chat/completions', body, {
       headers: {
         'Authorization': `Bearer ${process.env.ARK_OPENAI_API_KEY}`
       }
     })
-    print(JSON.parse(events_response.data.choices[0].message.content).events as GameEvent[])
-    if (JSON.parse(events_response.data.choices[0].message.content).maintance.length > 0) {
-      events = JSON.parse(events_response.data.choices[0].message.content).maintance as GameEvent[]
-    } else {
-      events = JSON.parse(events_response.data.choices[0].message.content).events as GameEvent[]
-    }
-    print("EVENTS")
-    print(events)
+    print(events_response.data.choices[0].message)
+    result = JSON.parse(events_response.data.choices[0].message.content)
+    print(result)
   } catch (error) {
     print('error')
     print((error as any))
-    events = []
   }
-
-  let summary = ""
-  // Set prompt
-  prompt = `以下是手游《明日方舟》的公告网页 HTML，请详细总结公告内容，以 JSON 格式输出（模板：{"summary": ""}）。如果页面只有一张带链接的图片，summary 请返回空字符串。`
-  body = {
-    "model": "gpt-3.5-turbo-0301",
-    "messages": [
-      {
-        "role": "system",
-        "content": prompt
-      },
-      {
-        "role": "user",
-        "content": content
-      }
-    ]
-  }
-  try {
-    let summary_response = await axios.post('https://api.openai.com/v1/chat/completions', body, {
-      headers: {
-        'Authorization': `Bearer ${process.env.ARK_OPENAI_API_KEY}`
-      }
-    })
-    summary = JSON.parse(summary_response.data.choices[0].message.content).summary
-  } catch(error) {
-    print('error')
-    summary = ""
-  }
-
-  let result = {
-    summary,
-    events
-  }
-  print("RESULT")
-  print(result)
   return result
 }
 
